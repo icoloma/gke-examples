04d217bdf8ab0c7fe59aa779a97b5ffcb0764727
Nacho Coloma <icoloma@gmail.com>
2016-08-17 21:24:11 +0200

    Add cleanup of changes to local files

--- federation/cleanup.sh
+++ federation/cleanup.sh
@@ -1,3 +1,5 @@
 # Cleanup
 gcloud container clusters delete icoloma-eu --zone europe-west1-b 
 gcloud container clusters delete icoloma-us --zone us-east1-b
+
+git checkout -- clusters/icoloma-eu.yaml clusters/icoloma-us.yaml deployments/federation-apiserver.yaml
