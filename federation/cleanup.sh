e38c0641968d8bfd494bb3259ecde429492bf077
Nacho Coloma <icoloma@gmail.com>
2016-11-24 12:04:34 +0100

    Added cleanup, stopping both clusters

--- federation/cleanup.sh
+++ federation/cleanup.sh
@@ -1,8 +1,17 @@
 # Cleanup
 
+
+# Remove everything
 kubectl --context federation-cluster delete rs nginx
 kubectl --context federation-cluster delete rs nginx-balanced
+kubectl --context federation-cluster delete service nginx
 
-gcloud container clusters delete icoloma-eu --zone europe-west1-b --async
-gcloud container clusters delete icoloma-us --zone us-east1-b
-rm -rf _output
+# Consider stopping your federated cluster instead of deleting it
+gcloud container clusters resize icoloma-eu --zone europe-west1-b --size=0
+gcloud container clusters resize icoloma-us --zone us-east1-b --size=0
+
+
+# Uncomment to effectively remove
+# gcloud container clusters delete icoloma-eu --zone europe-west1-b --async
+# gcloud container clusters delete icoloma-us --zone us-east1-b
+# rm -rf _output
