1afd7c1f18e1c9ca86abe77c47a8646f337b70b6
Nacho Coloma <icoloma@gmail.com>
2016-11-24 09:46:01 +0100

    Added the nginx service

--- federation/script-federated.sh
+++ federation/script-federated.sh
@@ -8,7 +8,6 @@
 # Check that everything is working fine
 kubectl --context=federation-cluster get clusters
 
-
 alias show_contexts='for c in $(kubectl config view -o jsonpath='{.contexts[*].name}'); do echo $c; done'
 euContext=$(show_contexts | grep icoloma-eu)
 usContext=$(show_contexts | grep icoloma-us)
@@ -20,20 +19,25 @@ watch kubectl --context=${usContext} get pods
 
 # this should throw error. The federation cluster is not 100% API compatible, since 
 # some things do not make sense 
-watch kubectl --context=federation-cluster get pods
+kubectl --context=federation-cluster get pods
 
 # Create the service on the federation cluster
 kubectl --context=federation-cluster \
   create -f lab/nginx-rs.yaml
 kubectl --context=federation-cluster \
   create -f lab/nginx-balanced.yaml
+kubectl --context=federation-cluster \
+  create -f lab/nginx-service.yaml
+
+# See the public IPs
+kubectl --context=federation-cluster \
+  describe services nginx
 
 # At any point, to debug the federated controller manager or api server
 kubectl logs -f --namespace=federation `kubectl --namespace federation get pods | grep controller-manager | cut -f 1 -d ' '`
 kubectl logs -f --namespace=federation `kubectl --namespace federation get pods | grep api | cut -f 1 -d ' '` -c apiserver
 
 # Create the Deployments
-
 kubectl --context=gke_glass-turbine-504_us-east1-b_icoloma-us create -f deployments/nginx.yaml 
 kubectl --context=gke_glass-turbine-504_us-east1-b_icoloma-us create -f deployments/nginx.yaml 
 
