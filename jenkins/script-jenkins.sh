3c81d29e77fd473f63ec764ebad30e3e4dc684c1
Nacho Coloma <icoloma@gmail.com>
2017-04-30 16:54:59 +0200

    Cleanup of steps in the demo

--- jenkins/script-jenkins.sh
+++ jenkins/script-jenkins.sh
@@ -1,55 +1,58 @@
 # Kubernetes + Jenkins demo
 
+Before the demo:
+
 ```bash
-# pre-work: create TLS certificate and key
-openssl req -x509 -newkey rsa:2048 -keyout cert/key.pem -out cert/cert.pem -days 3000 -nodes
+# Create service account for the demo
+# TODO: not working with the CLI yet, must be done with the web console instead (JSON key)
+# gcloud iam service-accounts keys create jenkins-demo-service-account.json --iam-account=jenkins-demo-service-account@icoloma-42.iam.gserviceaccount.com 
 
-# pre-work: create TLS secret
-kubectl create secret tls jenkins.cluster.local --cert=cert/cert.pem --keys=cert/key.pem
+# Assign permissions to the service account
+gcloud projects add-iam-policy-binding icoloma-42 --member serviceAccount:jenkins-demo-service-account@icoloma-42.iam.gserviceaccount.com --role roles/editor
 
+# Mount the JSON file as secret
+kubectl create secret generic jenkins-demo-secrets --from-file=./jenkins-demo-service-account.json
 
+# Create a separate cluster for the application
+gcloud container clusters create demo-dev --num-nodes=2
 
+```
 
+During the demo:
 
-# Show the name of the jenkins chart
+```bash
+# Show the jenkins chart to deploy
 helm search jenkins
 
 # Inspect the contents of the package
 helm inspect stable/jenkins
 helm inspect values stable/jenkins
 
-# Install using our overriding values
+# Install using our own values
 helm install -f values.yaml stable/jenkins --name democi
+./get-pass.sh
 
 # See the status of the deployment
 helm status <release-name>
 
 # Upgrade the deployment
 helm upgrade democi stable/jenkins --recreate-pods --values values.yaml
+```
 
-````
-
-Get the password from the command line with `./get-pass.sh`
 Open http://$SERVICE_IP:8777/login
-Put number of executor in the master config greater than 0 (e.g. 3)
-
-
-
-
-
-
 Create multibranch or single branch pipeline. 
 Add branch source, use https://github.com/icoloma/node-demo-app.git
-
 Show Jenkinsfile
-Show plugins in values.yaml and in the Jenkins console
 
+<!--
+TODO: 
+Do we need to put number of executor in the master config greater than 0? (e.g. 3)
 
+# create TLS certificate and key
+openssl req -x509 -newkey rsa:2048 -keyout cert/key.pem -out cert/cert.pem -days 3000 -nodes
 
-Use travis CI command line?
+# create TLS secret
+kubectl create secret tls jenkins.cluster.local --cert=cert/cert.pem --keys=cert/key.pem
 
-SSL!!!!!!!!!!
+-->
 
-
-
-Spinnaker on Cloud Launcher
