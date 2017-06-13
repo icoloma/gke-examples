5e042e6268e29eb7c2c5b7f76db2a48333c757f3
Nacho Coloma <icoloma@gmail.com>
2017-06-13 21:45:08 +0200

    Add some steps in the way

--- jenkins/script-jenkins.sh
+++ jenkins/script-jenkins.sh
@@ -3,6 +3,9 @@
 Before the demo:
 
 ```bash
+# create two clusters, one for jenkins and the other for the deployed app
+
+
 # Create service account for the demo
 # TODO: not working with the CLI yet, must be done with the web console instead (JSON key)
 # gcloud iam service-accounts keys create jenkins-demo-service-account.json --iam-account=jenkins-demo-service-account@icoloma-42.iam.gserviceaccount.com 
@@ -30,10 +33,15 @@ helm inspect values stable/jenkins
 
 # Install using our own values
 helm install -f values.yaml stable/jenkins --name democi
-./get-pass.sh
 
 # See the status of the deployment
-helm status <release-name>
+helm status democi
+
+# Get the password of the admin user
+./get-pass.sh
+
+# inspect the generated jenkins config
+kubectl exec -it democi-jenkins-1138451357-bhfpc cat /var/jenkins_config/config.xml
 
 # Upgrade the deployment
 helm upgrade democi stable/jenkins --recreate-pods --values values.yaml
@@ -45,3 +53,13 @@ Add branch source, use https://github.com/icoloma/node-demo-app.git
 Show Jenkinsfile
 
 
+## GCB demo
+
+In the node-demo-app/ folder:
+
+* Show cloudbuild.yaml
+* Make a change, and run:
+
+``` bash
+git commit -m 'Change' && git push && git tag v1.0 --force && git push --tags --force
+```
